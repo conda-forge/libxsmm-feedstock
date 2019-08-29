896bfc99c90a17b8f1fafbd36e75fcffcc7fdc00
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-08-29 18:28:39 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-make -j${CPU_COUNT} PREFIX=${PREFIX} STATIC=0
+if [[ $HOST == *"apple"* ]]; then
+  make -j${CPU_COUNT} PREFIX=${PREFIX}
+else
+  make -j${CPU_COUNT} PREFIX=${PREFIX} STATIC=0
+fi
 make -j${CPU_COUNT} install
 make -j${CPU_COUNT} test
 make -j${CPU_COUNT} test-all
