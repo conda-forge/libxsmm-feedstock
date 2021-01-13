4253835250493952b830d7e88ad265b81ff8171e
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-01-14 00:00:08 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ export LDFLAGS="${LDFLAGS} -lblas"
 
 if [[ $HOST == *"apple"* ]]; then
   export LDFLAGS="${LDFLAGS} -lomp"
+  export CPU_COUNT=1
   make -j${CPU_COUNT} PREFIX=${PREFIX}
 else
   make -j${CPU_COUNT} PREFIX=${PREFIX} STATIC=0
